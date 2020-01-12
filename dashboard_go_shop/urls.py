08e12164ebd5ff66a21d74752e418cb9723cdd95
Jesus Millan <jgmc3012@gmail.com>
2020-01-12 19:59:14 -0400

    :beetle: fix: configuracion de archivos estaticos

--- dashboard_go_shop/urls.py
+++ dashboard_go_shop/urls.py
@@ -19,8 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import index
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
     path('orders/', include('store.orders.urls')),
+    path('', index, name='index')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
