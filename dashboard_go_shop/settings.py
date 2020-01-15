2003cac9192209e3549873c2c4a2d018022aca0e
Jesus Millan <jgmc3012@gmail.com>
2020-01-15 14:27:41 -0400

    :recycle: refactor: Cambio en los dominios permitidos

--- dashboard_go_shop/settings.py
+++ dashboard_go_shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '164.68.124.160', '.goshop27.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '164.68.124.160', '.threepersonteam.com']
 
 
 # Application definition
