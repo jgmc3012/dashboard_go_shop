a62066e99651f9b7224ad552be227f37a37f14ea
Jesus Millan <jgmc3012@gmail.com>
2020-01-15 22:18:53 -0400

    :recycle: refactor: comando para actualizar publicaciones

--- store/management/commands/get_mcos.py
+++ store/management/commands/get_mcos.py
@@ -5,17 +5,15 @@ import csv
 from datetime import datetime
 
 class Command(BaseCommand):
-    help = 'Crea un CSV con el MLV y el MCO de cada producto'
+    help = 'Recibe por parametro el id de las categoria de MELI Colombia \
+para jalar la data a nuestro MarketPlace'
 
     def add_arguments(self,parser):
-            parser.add_argument('categories_id', nargs='+', type=str)
+        parser.add_argument('categories_id', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        start = datetime.now()
-        
         categories_id = options['categories_id']
         for category_id in categories_id:
-            time_parse = datetime.now()
             mcos = get_mcos(category_id)
             print(f'{category_id} --> {len(mcos)}')
             with open(f'csv/{category_id}.csv', 'w', newline='') as csvfile:
@@ -23,7 +21,3 @@ class Command(BaseCommand):
                                     ,quoting=csv.QUOTE_MINIMAL)
                 for mco in mcos:
                     spamwriter.writerow([mco])
-            print(f'Tiempo de la categoria {category_id} {datetime.now()-time_parse}')
-
-        print(f'Tiempo total {datetime.now()-start}')
-        
