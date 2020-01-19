b27b44efb102b9ef2b5a26c136e0269b1fd915b8
Jesus Millan <jgmc3012@gmail.com>
2020-01-19 19:37:52 -0400

    :recycle: refactor: Principio DRY aplicado en archivos de js

---
+++ static/js/dashboard/dashboard.js
@@ -0,0 +1,56 @@
+alertInfoHTML = (message, type, mtype) => `
+<div class="alert alert-${type} fade in alert-dismissible show">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true" style="font-size:20px">×</span>
+    </button>
+    <strong>${mtype}</strong>   ${message}
+</div>
+`
+function createTemplate(HTMLString) {
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+    return html.body.children[0];
+    }
+
+function insertElement(Selectorcontaner, HTMLString) {
+    let container = document.querySelector(Selectorcontaner)
+    container.children[0].remove();
+    element = createTemplate(HTMLString);
+    container.append(element)
+}
+
+function sendData(data, url, selectorModal) {
+    const method = 'POST'
+    const headers = {
+        "X-CSRFToken": getCookie('csrftoken'),
+        'Content-Type': 'application/json',
+        "X-Requested-With": "XMLHttpRequest",
+    }
+    const body = JSON.stringify(data)
+
+    const modal = document.querySelector(selectorModal)
+    modal.click()
+    fetch(url,{method,headers,body})
+    .then( response => response.json())
+    .then( data => {
+        const type = data.ok ? 'success':'danger'
+        const mtype = data.ok ? 'Buen Trabajo':'Error'
+        const HTMLString = alertInfoHTML(data.msg, type, mtype)
+        insertElement('#InfoMsg', HTMLString)
+        if (data.ok) {
+            itemlistElement = document.querySelector(`[order_id='${orderId}']`)
+            itemlistElement.remove()
+        }
+    })
+}
+
+function getJsonFromForm(selector) {
+    elements = document.querySelectorAll(selector)
+    data = {}
+    elements.forEach( element => {
+        key = element.getAttribute('name')
+        value = element.value
+        data[key] = value
+    } )
+    return data
+} 
--- static/js/dashboard/next_state.js
+++ static/js/dashboard/next_state.js
@@ -1,78 +1,19 @@
-alertInfoHTML = (message, type, mtype) => `
-<div class="alert alert-${type} fade in alert-dismissible show">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true" style="font-size:20px">×</span>
-    </button>
-    <strong>${mtype}</strong>   ${message}
-</div>
-`
-
-function createTemplate(HTMLString) {
-    const html = document.implementation.createHTMLDocument();
-    html.body.innerHTML = HTMLString;
-    return html.body.children[0];
-    }
-
-function insertElement(Selectorcontaner, HTMLString) {
-    let container = document.querySelector(Selectorcontaner)
-    container.children[0].remove();
-    element = createTemplate(HTMLString);
-    container.append(element)
-}
-
-function sendData(state, orderId) {
-    const method = 'POST'
-    const headers = {
-        "X-CSRFToken": getCookie('csrftoken'),
-        'Content-Type': 'application/json',
-        "X-Requested-With": "XMLHttpRequest",
-    }
-    const body = JSON.stringify(getJsonFromForm("[api='nextState']"))
-    let url
+function selectUrl(state, orderId) {
     switch (state + 1) {
         case 1:
-            url = `${window.location.origin}/orders/api/new_pay/${orderId}`
-            break;
+            return `${window.location.origin}/orders/api/new_pay/${orderId}`
+            break
         case 2:
-            url = `${window.location.origin}/orders/api/buys/done/${orderId}`
-            break;
+            return `${window.location.origin}/orders/api/buys/done/${orderId}`
+            break
         case 3:
-            url = `${window.location.origin}/orders/api/provider_deliveries/${orderId}`
-            break;
+            return `${window.location.origin}/orders/api/provider_deliveries/${orderId}`
+            break
         case 6:
-            url = `${window.location.origin}/orders/api/complete_order/${orderId}`
-            break;
+            return `${window.location.origin}/orders/api/complete_order/${orderId}`
+            break
     }
-
-    const modal = document.getElementById('stateModal')
-    modal.click()
-    fetch(url,{method,headers,body})
-    .then( response => response.json())
-    .then( data => {
-        const type = data.ok ? 'success':'danger'
-        const mtype = data.ok ? 'Buen Trabajo':'Error'
-        const HTMLString = alertInfoHTML(data.msg, type, mtype)
-        insertElement('#InfoMsg', HTMLString)
-        if (data.ok) {
-            itemlistElement = document.querySelector(`[order_id='${orderId}']`)
-            itemlistElement.remove()
-        }
-    })
 }
-
-function getJsonFromForm(selector) {
-    elements = document.querySelectorAll(selector)
-    data = {}
-    elements.forEach( element => {
-        key = element.getAttribute('name')
-        value = element.value
-        data[key] = value
-    } )
-    return data
-} 
-
-
-///////////////////////////HASTA AQUI CODIGO REPETIDO///////////////////////////////////
 
 const formNextState = (state, orderId ) => {
     switch (state+1) {
@@ -134,12 +75,9 @@ const formNextState = (state, orderId ) => {
             break
     }
 }
-
-
 // BEGIN
 
 btnsNextState = document.querySelectorAll('[data-target="#stateModal"]')
-
 btnsNextState.forEach( (btn) => {
     btn.addEventListener('click', (event) => {
         let orderId = event.target.getAttribute('id')
@@ -147,12 +85,14 @@ btnsNextState.forEach( (btn) => {
         const HTMLString = formNextState(state,orderId)
         if (HTMLString) {
             insertElement('#formContainer', HTMLString)
-    
+
             btnRequest = document.getElementById('btnRequestNextState')
             btnRequest.addEventListener( 'click' , (event) => {
                 state = parseInt(event.target.getAttribute('state'))
                 orderId = event.target.getAttribute('orderId')
-                sendData(state ,orderId)
+                let url = selectUrl(state, orderId)
+                let data = getJsonFromForm("[api='nextState']")
+                sendData(data ,url, '#stateModal')
             })
         }
     })
