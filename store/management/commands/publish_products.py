b02a149b03b588df0703fa6fcd173a9f085601aa
Jesus Millan <jgmc3012@gmail.com>
2020-01-29 21:34:35 -0400

    :recycle: refactor: feedback en el output

--- store/management/commands/publish_products.py
+++ store/management/commands/publish_products.py
@@ -3,14 +3,18 @@ from django.core.management.base import BaseCommand, CommandError
 from store.products.models import Product
 from store.store import Store
 from concurrent.futures import ThreadPoolExecutor
-
+from datetime import datetime
+import logging
 
 class Command(BaseCommand):
     help = 'Publica nuevos producto en las cuenta de mercado libre'
 
     def handle(self, *args, **options):
         store = Store()
+        start = datetime.now()
+        logging.info('Consultando la base de datos')
         products = Product.objects.filter(sku=None,quantity__gt=2)
+        logging.info(f'Fin de la consulta, tiempo de ejecucion {datetime.now()-start}')
 
         with ThreadPoolExecutor(max_workers=5) as executor:
             results = executor.map(store.publish, products)
