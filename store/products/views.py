928f113459788d3172d1fa0bcbf32e69967f00c4
Jesus Millan <jgmc3012@gmail.com>
2020-01-31 15:43:49 -0400

    :beetle: fix: errores por copy/paste

--- store/products/views.py
+++ store/products/views.py
@@ -5,6 +5,9 @@ from django.http import JsonResponse
 from .models import Product
 from meli_sdk.models import BulkCreateManager
 from store.products.models import Product
+import re
+from store.store import Store
+import logging
 
 @login_required
 def get_url_provider(request, sku):
@@ -29,8 +32,10 @@ def filter_bad_products():
     bulk_mgr = BulkCreateManager()
 
     products = Product.objects.filter(available=True)
+
+    store = Store()
     for product in products:
-        if re.search(self.pattern_bad_words, product.title.upper()):
+        if re.search(store.pattern_bad_words, product.title.upper()):
             msg = f'{product}. Contiene palabras prohibidas.'
             logging.warning(msg)
             product.available = False
